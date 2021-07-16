a99508fb2d1e297db8021be432d06b45986ee616
Mutasem <mutdmour@gmail.com>
2021-07-16 11:22:01 +0200

    add watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,29 @@
 'use strict';
 
-const { series, src, dest } = require('gulp');
+const gulp = require('gulp');
 const sass = require('gulp-dart-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 
 function compile() {
-  return src('./src/*.scss')
+  return gulp.src('./src/*.scss')
     .pipe(sass.sync())
     .pipe(autoprefixer({
       browsers: ['ie > 9', 'last 2 versions'],
       cascade: false
     }))
     .pipe(cleanCSS())
-    .pipe(dest('./lib'));
+    .pipe(gulp.dest('./lib'));
 }
 
 function copyfont() {
-  return src('./src/fonts/**')
-    .pipe(dest('./lib/fonts'));
+  return gulp.src('./src/fonts/**')
+    .pipe(gulp.dest('./lib/fonts'));
 }
 
-exports.build = series(compile, copyfont);
+gulp.task('build', gulp.series([compile, copyfont]))
+
+gulp.task('watch', gulp.series(['build', () => {
+	gulp.watch('./src/**/*.scss', gulp.series(['build']));
+}]));
+
