64eeade73eb4e3ef6cf33f8152f95ac6ff9f9be8
Mutasem <mutdmour@gmail.com>
2021-07-18 13:14:49 +0200

    implement hsl colors

--- gulpfile.js
+++ gulpfile.js
@@ -6,18 +6,65 @@ const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 
 const StyleDictionary = require('style-dictionary')
-.extend({
-  source: ['src/tokens/**/*.json'],
-  platforms: {
-    scss: {
-      transforms: ["attribute/cti", "name/cti/kebab", "size/rem"],
-      buildPath: 'src/',
-      files: [{
-        destination: '_tokens.scss',
-        format: 'css/variables'
-      }]
-    }
-  }
+	.extend({
+		source: ['src/tokens/**/*.json'],
+		platforms: {
+			scss: {
+				transforms: ["attribute/cti", "name/cti/kebab", "size/rem"],
+				buildPath: 'src/',
+				files: [{
+					destination: '_tokens.scss',
+					format: 'css/custom'
+				}]
+			}
+		}
+	});
+
+
+const {
+	fileHeader,
+	formattedVariables
+} = StyleDictionary.formatHelpers;
+
+StyleDictionary.registerFormat({
+	name: 'css/custom',
+	formatter: function ({
+		dictionary,
+		file,
+		options
+	}) {
+		const {
+			outputReferences
+		} = options;
+		dictionary.allTokens = dictionary.allTokens.reduce((accu, token) => {
+			if (token.attributes.category === 'color' && token.value.startsWith('hsl(')) {
+				const colors = token.value.replace('hsl(', '').replace(')', '').split(',');
+				const parts = ['h', 's', 'l'];
+				accu = accu.concat(colors.map((value, i) => {
+					return {
+						...token,
+						value,
+						name: `${token.name}-${parts[i]}`,
+					};
+				}));
+				return accu;
+			}
+
+			accu.push(token);
+			return accu;
+		}, []);
+		
+		return fileHeader({
+				file
+			}) +
+			':root {\n' +
+			formattedVariables({
+				format: 'css',
+				dictionary,
+				outputReferences
+			}) +
+			'\n}\n';
+	}
 });
 
 function compile() {
@@ -32,8 +79,8 @@ function compile() {
 }
 
 function compileTokens(done) {
-  StyleDictionary.buildAllPlatforms();
-  done();
+	StyleDictionary.buildAllPlatforms();
+	done();
 };
 
 function copyfont() {
